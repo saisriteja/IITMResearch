982e4d906379c295df7757e7067617e65eb9dcfd
saisritejakuppa <saisritejakuppa@gmail.com>
2024-01-02 20:20:03 +0530

    inital comit

--- pages/pages_1.py
+++ pages/pages_1.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def page2():
-    st.markdown("# Page 2 ❄️")
+    st.markdown("# Page 2 yo ❄️")
     st.sidebar.markdown("# Page 2 ❄️")
--- pages/pages_2.py
+++ pages/pages_2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 def page3():
-    st.markdown("# Page 3 🎉")
+    st.markdown("# Page 3 okay 🎉")
     st.sidebar.markdown("# Page 3 🎉")
