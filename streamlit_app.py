07eaa86a828fa65f0c61bb0b0e9d039ddff24139
saisritejakuppa <saisriteja@detecttechnologies.com>
2024-01-05 10:25:29 +0530

    repo restructure

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,39 +4,26 @@ from src.reading_readme import ReadingReadMe
 
 
 def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+    st.title("Welcome to my website!")
+    st.write("This is a website where I will be posting my research papers and insights out of it.")
 
-
-
-def diffusion(path = 'research_papers/diffusion'):
+def page_name(path = 'research_papers/diffusion'):
 
     # drop down menu
     file_names = os.path.join(path, '*.md')
     file_names = os.listdir(path)
-
     selected_file = st.sidebar.selectbox("Select a file", file_names)
-
     selected_file = os.path.join(path, selected_file)
-
     file_info = ReadingReadMe(selected_file)
-
     file_info.run()
-
-    # # show the dict in the streamlit
-    # for key, value in file_info.information.items():
-    #     st.markdown(f"## {key}")
-    #     for point in value:
-    #         st.markdown(f"- {point}")
-
     file_info.get_markdown()
-
 
 
 
 page_names_to_funcs = {
     "Main Page": main_page,
-    "Diffusion": diffusion,
+    "Diffusion": page_name('research_papers/diffusion'),
+    "DeepFakeResearch": page_name('research_papers/DeepFakeResearch'),
 }
 selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
